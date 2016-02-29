e752769e9e07de7e2e80918570c94b9fdd07eb55
Charlie <cm9083@gmail.com>
2016-02-29 23:53:06 +0000

    Added files via upload
    Added kill command for bot operators, and alphabetized the commands.

--- bot.py
+++ bot.py
@@ -16,36 +16,31 @@ client.login("username","password")
 @client.async_event
 def on_message(message):
     #General stuff
-    if message.content.startswith("!command") or message.content.startswith("!commands".casefold()):
+    if message.content.startswith("!commands") or message.content.startswith("!command".casefold()):
         yield from client.send_message(message.channel, "A list of current commands: you really expect to list all these? Bloody hell.")
+
     if message.content.startswith("!hello".casefold()):
         yield from client.send_message(message.channel, "Hello " + message.author.name)
+        
     if message.content.startswith("!bye".casefold()):
         yield from client.send_message(message.channel, "Bye " + message.author.name)
-    if message.content.startswith("!meme".casefold()):
-        yield from client.send_message(message.channel, "Dank memes")
-    if message.content.startswith("!kek".casefold()) or message.content.startswith("!topkek".casefold()):
-        yield from client.send_message(message.channel, "top kek xD")
+        
+    if message.content.startswith("!g4g".casefold()):
+        yield from client.send_message(message.channel, "http://www.gamingforgood.net/s/r00kieoftheyear#donate MingLee")
+        
     if message.content.startswith("!itis".casefold()):
         number = randint(1,10)
         if number % 3 == 0:
             yield from client.send_message(message.channel, "Is it?")
         else:
             yield from client.send_message(message.channel, "It isn't")
+            
+    if message.content.startswith("!kek".casefold()) or message.content.startswith("!topkek".casefold()):
+        yield from client.send_message(message.channel, "top kek xD")
+        
+    if message.content.startswith("!meme".casefold()):
+        yield from client.send_message(message.channel, "Dank memes")
     
-    if message.content.startswith("!age".casefold()):
-        yield from client.send_message(message.channel, "PedoBear")
-    #People
-    if message.content.startswith("!chris".casefold()):
-        yield from client.send_message(message.channel, "Abusive uncle chris DansGame")
-    if message.content.startswith("!eladia".casefold()):
-        yield from client.send_message(message.channel, "We miss you BibleThump")
-    if message.content.startswith("!kieran".casefold()):
-        yield from client.send_message(message.channel, "http://i.imgur.com/37274Z1.jpg oi oi kieran")
-    if message.content.startswith("!sogyoh".casefold()):
-        yield from client.send_message(message.channel, "TriHard I am so fucking zooted right now CiGrip")
-    if message.content.startswith("!g4g".casefold()):
-        yield from client.send_message(message.channel, "http://www.gamingforgood.net/s/r00kieoftheyear#donate MingLee")
     if message.content.startswith("!selfdestruct".casefold()):
         for i in range(10,-1,-1):
             if i == 0:
@@ -53,22 +48,48 @@ def on_message(message):
                 break
             yield from asyncio.sleep(1)
             yield from client.send_message(message.channel, "{}".format(i))
+    
+    if message.content.startswith("!age".casefold()):
+        yield from client.send_message(message.channel, "PedoBear")
+        
+    #People
+        
+    if message.content.startswith("!chris".casefold()):
+        yield from client.send_message(message.channel, "Abusive uncle chris DansGame")
+        
+    if message.content.startswith("!eladia".casefold()):
+        yield from client.send_message(message.channel, "We miss you BibleThump")
+        
+    if message.content.startswith("!kieran".casefold()):
+        yield from client.send_message(message.channel, "http://i.imgur.com/37274Z1.jpg oi oi kieran")
+        
+     if message.content.startswith("!nick".casefold()):
+        yield from client.send_message(message.channel, "NIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIICK! Kreygasm")
+        
+    if message.content.startswith("!sogyoh".casefold()):
+        yield from client.send_message(message.channel, "TriHard I am so fucking zooted right now CiGrip")
             
     #GIFs
+            
     if message.content.startswith("!flex".casefold()):
         yield from client.send_message(message.channel, "http://i.imgur.com/YdCl7E8.gif")
+        
+    if message.content.startswith("!fuckingmental".casefold()):
+        yield from client.send_message(message.channel, "http://i.imgur.com/GiG4nPn.gif")
+        
     if message.content.startswith("!rarelola".casefold()):
         yield from client.send_message(message.channel, "http://i.imgur.com/yz6c2RE.gif")
-    if message.content.startswith("!fuckingmetal".casefold()):
-        yield from client.send_message(message.channel, "http://i.imgur.com/GiG4nPn.gif")
+        
+    if message.content.startswith("!shoe".casefold()):
+        yield from client.send_message(message.channel, "http://i.imgur.com/qEMJJTP.gif")
     
     
-    #ad infinitum
-    #kill (only available to the person running the bot)
+    #kill (only available to bot operators)
     if message.content.startswith("!kill".casefold()):
         if message.author.name == "TheWizoid" and message.author.id == "99916696600461312":
             yield from client.send_message(message.channel, "Barry Bot going down BibleThump /")
             os._exit(5)
+    #ad infinitum
             
 @client.async_event
 #Displays login name/id
