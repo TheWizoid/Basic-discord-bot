2804b3488cf8b4fa6c3e18cad5d9b9b22c014ecd
Charlie <cm9083@gmail.com>
2016-03-27 04:13:06 +0100

    clarity

--- message_counting.py
+++ message_counting.py
@@ -1,31 +1,41 @@
 import pickle
 
 if __name__ == "__main__":
+    """
+    Let's you input the name of a server that has had its chat logged by the bot
+    and have it create a file of all users and their messages sent.
+    """
     server = ""
     while server != "q":
         server = input("What is the name of the server you will be counting? ").lower()
-
         try:
             chatlog = open("{}_chatlog.txt".format(server),"r")
         except FileNotFoundError:
             if server != "q":
                 print("That server doesn't exist.")
             chatlog = ""
+
         try:
             message_amount = pickle.load(open("{}_messages.txt".format(server),"rb"))
         except FileNotFoundError:
             message_amount = {"barry chuckle bot":0}
-    
+
+        """
+        Messages are saved in the format [YYYY-MM-DD HH:MM:SS]User:Message
+        Example: [2016-03-02 19:06:23]TheWizoid:test
+        """
         for line in chatlog:
             if line[0] == "[":
                 line = line.strip()
                 name = ""
                 name_start = line.find("]")+1
                 name_end = line.find(":",19)
+                
                 for i in range(name_start, name_end):
                     name += line[i]
                 name = name.lower()
                 print(name)
+                
                 try:
                     message_amount[name] += 1
                 except KeyError:
